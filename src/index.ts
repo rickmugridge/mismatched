1d1c6255a799f8b4aa1736b4bfda7dc491cf7b88
rickmugridge <rick.mugridge@hypr.co.nz>
2019-11-15 06:52:46 +1300

    1.2.8 Export MatchResult for thespian.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {match} from "./match";
 export {assertThat, assertException} from "./assertThat";
+export {MatchResult} from "./MatchResult";
 export {DiffMatcher} from "./matcher/DiffMatcher";
 export {PrettyPrinter} from "./prettyPrint/PrettyPrinter";
