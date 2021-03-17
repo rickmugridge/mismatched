d368bc8f7a76ea2d6dfa214b55b5cc447bfd9f25
rickmugridge <rick.mugridge@hypr.co.nz>
2021-03-18 12:47:50 +1300

    Use TS result type of 'v is T'

--- src/ofType.ts
+++ src/ofType.ts
@@ -1,16 +1,16 @@
-function isUndefined(v: any): boolean {
+function isUndefined(v: any): v is undefined {
     return typeof v === "undefined";
 }
 
-function isNull(v: any): boolean {
+function isNull(v: any): v is null {
     return v === null;
 }
 
-function isObject(v: any): boolean {
+function isObject(v: any): v is object {
     return v !== null && !isArray(v) && typeof v === "object" && !(v instanceof RegExp);
 }
 
-function isArray(v: any): boolean {
+function isArray(v: any): v is any[] {
     return Array.isArray(v);
 }
 
@@ -18,19 +18,19 @@ function isFunction(v: any): boolean {
     return typeof v === "function";
 }
 
-function isSet(v: any): boolean {
+function isSet(v: any): v is Set<any> {
     return v instanceof Set;
 }
 
-function isMap(v: any): boolean {
+function isMap(v: any): v is Map<any, any> {
     return v instanceof Map;
 }
 
-function isString(v: any): boolean {
+function isString(v: any): v is string {
     return typeof v === 'string' || v instanceof String;
 }
 
-function isNumber(v: any): boolean {
+function isNumber(v: any): v is number {
     return typeof v === 'number'; // && isFinite(value);
 }
 
@@ -38,7 +38,7 @@ function isNaN(v: any): boolean {
     return v != v; // Avoid weird JS coercing that make isNaN() and Number.isNaN() unreliable
 }
 
-function isBoolean(v: any): boolean {
+function isBoolean(v: any): v is boolean {
     return typeof v === 'boolean';
 }
 
@@ -46,15 +46,15 @@ function isRegExp(v: any): boolean {
     return v instanceof RegExp;
 }
 
-function isSymbol(v: any): boolean {
+function isSymbol(v: any): v is symbol {
     return typeof v === 'symbol';
 }
 
 function isMatcher(v: any): boolean {
-    return isObject(v) && !isUndefined(v.matches) && isFunction(v.matches);
+    return isObject(v) && !isUndefined((v as any).matches) && isFunction((v as any).matches);
 }
 
-function isError(v: any): boolean {
+function isError(v: any): v is Error {
     return v instanceof Error;
 }
 
